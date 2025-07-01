da4ec19455934da96bc52c966cfa4d1cd9c7effb
Ashutoshh <ashutoshh@linux.com>
2025-07-01 14:43:33 +0530

    updates kafka

--- day2/kafka-client.py
+++ day2/kafka-client.py
@@ -1,4 +1,11 @@
-from kafka import KafkaProducer, KafkaConsumer
+from kafka import  KafkaConsumer,KafkaAdminClient
+from kafka.admin import NewTopic
+
+# Initialize a Kafka admin client
+admin_client = KafkaAdminClient(
+    bootstrap_servers='3.1.143.30:9092',
+    client_id='my_admin_client'
+)
 # Initialize a Kafka consumer without any topic
 consumer = KafkaConsumer(
     bootstrap_servers='3.1.143.30:9092');
@@ -10,3 +17,11 @@ print("Available topics:", topics)
 ## list using for loop
 for topic in topics:
     print("Topic:", topic)
+
+# Create a new topic
+new_topic = NewTopic(name='ashu-python-topic1', num_partitions=1,replication_factor=1)
+# Create the topic using the admin client
+admin_client.create_topics(new_topics=[new_topic], validate_only=False)
+
+
+                        
