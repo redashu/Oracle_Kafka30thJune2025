6add085fe96980092a096c717ab7170380be32f2
Ashutoshh <ashutoshh@linux.com>
2025-07-01 16:20:44 +0530

    updates kafka

--- day2/kafka-consumer.py
+++ day2/kafka-consumer.py
@@ -5,10 +5,16 @@ consumer = KafkaConsumer(
     bootstrap_servers='3.1.143.30:9092',
     client_id='ashu_consumer_client',  # Options to identify the consumer
     auto_offset_reset='earliest',  # Start reading from the earliest message
-    #enable_auto_commit=True,  # Automatically commit offsets
+    enable_auto_commit=True,  # Automatically commit offsets
+    consumer_timeout_ms=5000, # after 5 seconds of no message
     value_deserializer=lambda x: x.decode('utf-8')  # Deserialize messages as
 )
 
 # Consume messages from the topic
 for message in consumer:
     print(f"Received message: {message.value} from topic: {message.topic} at offset: {message.offset}")
+
+
+# close consumer 
+
+consumer.close()
